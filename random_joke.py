7abc00d4f85d62ed0b1433b443d145ac63ce23e3
vilius kulinas <vilius.kulinas@gmail.com>
2021-07-21 15:44:45 +0100

    return api response in json

--- random_joke.py
+++ random_joke.py
@@ -1,1 +1,24 @@
-# Flask App
+#Flask app
+from flask import Flask
+import requests
+
+
+
+app = Flask(__name__)
+
+url = 'https://official-joke-api.appspot.com/random_joke'
+response = requests.get(url)
+
+
+
+@app.route("/")
+def hello_world():
+    return "<h1>My first flask app!</h1>"
+
+@app.route("/random-joke")
+def return_joke():
+    return response.json()
+
+
+if __name__=='__main__':
+    app.run(debug=False)
