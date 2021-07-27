3fb56f29e5ce2a61bcee1d477925ea5e984e71ed
Vilius <vilius.kulinas@gmail.com>
2021-07-27 16:34:11 +0100

    few changes

--- random_joke.py
+++ random_joke.py
@@ -10,18 +10,14 @@ app = Flask(__name__)
 url = 'https://official-joke-api.appspot.com/random_joke'
 
 
-
-@app.route("/")
-def hello_world():
-    return "<h1>My first flask app!</h1>"
-
 @app.route("/random-joke")
 def return_joke():
     response = requests.get(url)
 
-    #return api data as dictionary
-    api_dict = json.loads(response.text)
+    #return api data as json
+    api_dict = response.json()
 
+    #using f string to print out values from json()
     return f"{api_dict['setup']}...{api_dict['punchline']}"
 
 
