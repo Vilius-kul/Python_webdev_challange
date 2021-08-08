f0f37e49df1989300c9ae6c599b858278f4ba4bc
Vilius <vilius.kulinas@gmail.com>
2021-08-08 15:20:30 +0100

    add random-5-jokes

--- random_joke.py
+++ random_joke.py
@@ -1,4 +1,6 @@
+import re
 from flask import Flask
+from werkzeug.datastructures import Range
 from clients import JokeApi
 
 app = Flask(__name__)
@@ -9,6 +11,18 @@ def return_joke():
     joke = JokeApi.get_random_joke()
     
     return f"{joke['setup']}...{joke['punchline']}"
+
+@app.route("/random-5-jokes")
+def return_5_jokes():
+    jokes = ""
+    for i in range(1,6):
+        jokes += return_joke()
+    return jokes
+    
+            
+
+
+
 
 
 if __name__=='__main__':
