09d1a1c195a5903360a367a394081ef1ada2dc93
vilius <vilius.kulinas@gmail.com>
2021-10-09 17:53:58 +0100

    4 tests passed

--- tests/test_multiple_jokes_request_params.py
+++ tests/test_multiple_jokes_request_params.py
@@ -1,4 +1,12 @@
+import pytest
+
 from src.schemas import MultipleJokesRequestParams
+
+
+@pytest.fixture
+def multipleJokesRequestParams(**kwargs):
+    return multipleJokesRequestParams(**kwargs)
+    
 
 def test_is_in_range_with_valid_range():
     # Arrange
@@ -15,14 +23,47 @@ def test_is_in_range_with_valid_range():
 
 
 def test_is_in_range_with_invalid_range():
-    # Arrange
-    valid_count = 25
     
-    # Act
-    MultipleJokesRequestParams(
-        count=valid_count,
-        language="pl"
-    )
+    with pytest.raises(Exception) as excinfo:
+        
+        # Arrange
+        not_valid_count = 25
+
+        # Act
+        MultipleJokesRequestParams(
+            count=not_valid_count,
+            language="pl"
+        )
     
     # Assert
+    assert "Out of range! Try 1 to 10." in str(excinfo.value)
+    
+    
+def test_is_in_lang_list():
+    
+    # Arrange
+    valid_language = "lt"
+
+    # Act
+    MultipleJokesRequestParams(
+            count=1,
+            language=valid_language
+        )
+    # Assert
     pass # does not raise ValueError
+
+
+def test_not_in_lang_list():
+    
+    with pytest.raises(Exception) as excinfo:
+        
+        # Arrange
+        not_valid_language = "cz"
+        
+        # Act
+        MultipleJokesRequestParams(
+            count=1,
+            language=not_valid_language
+        )
+    # Assert
+    assert "Wrong input!" in str(excinfo)
